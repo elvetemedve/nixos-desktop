29b4cbb52c02d503218a92af344c99cbac70c769
Géza Búza <bghome@gmail.com>
2025-02-02 00:02:22 +0100

    Install Steam

--- hosts/ThinkPadP16s/default.nix
+++ hosts/ThinkPadP16s/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   imports = [
+    ../../modules/gaming.nix
     ../../modules/webserver.nix
     ../../modules/system.nix
 
---
+++ modules/gaming.nix
@@ -0,0 +1,12 @@
+# Enable gaming
+{ config, pkgs, ... }:
+
+{
+  # Install Valve Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+}
