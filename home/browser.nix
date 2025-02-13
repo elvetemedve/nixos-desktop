7e641b56e98167ed27588a4f48a8a39a46af4255
Géza Búza <bghome@gmail.com>
2025-02-13 21:16:28 +0100

    Adjust Firefox user preferences

--- home/browser.nix
+++ home/browser.nix
@@ -19,9 +19,16 @@
       };
       settings = {
         "identity.fxaccounts.toolbar.enabled" = false; # Remove Mozilla Monitor from the toolbar
+
+        # Preserve data after closing the browser
         "privacy.clearOnShutdown.cookies" = false;
         "privacy.clearOnShutdown.history" = false;
         "privacy.clearOnShutdown.downloads" = false;
+        "privacy.clearOnShutdown.siteSettings" = false;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false;
+        "privacy.clearOnShutdown_v2.siteSettings" = false;
+        "privacy.sanitize.sanitizeOnShutdown" = false;
         "webgl.disabled" = false;
       };
     };
