e1831965ef43a31775fbd3683fcdd04b5ef3b02b
Géza Búza <bghome@gmail.com>
2025-02-19 00:25:17 +0100

    Mount SSHFS (Home Server)

--- home/storage.nix
+++ home/storage.nix
@@ -1,5 +1,9 @@
 { username, lib, osConfig, pkgs, ... }:
 {
+  home.file."ssh-config" = {
+    source = ./storage/ssh-config;
+    target = ".ssh/config";
+  };
   home.packages = with pkgs; [
     sshfs # Utility to mount SSH connection as filesystem
   ];
@@ -48,5 +52,42 @@
 
     };
   };
+
+  systemd.user.automounts."home-${username}-home_server" = {
+    Unit = {
+      Description = "Automount ~/home_server";
+    };
+
+    Automount = {
+      Where = "/home/${username}/home_server";
+      TimeoutIdleSec = "5min";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+  
+  systemd.user.mounts."home-${username}-home_server" = {
+    Unit = {
+      Description = "SSHFS (Home Server)";
+      Before = "remote-fs.target";
+    };
+
+    Mount = {
+      What = "${username}@nixos.lan:/mnt/storage";
+      Where = "/home/${username}/home_server";
+      Type = "fuse.sshfs";
+      Options = [ "_netdev" "rw" "nosuid" "default_permissions" "allow_other" "follow_symlinks" "idmap=user" "ServerAliveInterval=10" "ServerAliveCountMax=1" "ConnectTimeout=3" ];
+      ForceUnmount = true;
+      DirectoryMode = "0700";
+      TimeoutSec = 10;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
 }
 
