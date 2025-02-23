b845b7679f65b892d6acdb567e1431e13c28ee10
Géza Búza <bghome@gmail.com>
2025-02-23 01:24:46 +0100

    Remove SSHFS in favour of WebDav

--- home/storage.nix
+++ home/storage.nix
@@ -4,9 +4,6 @@
     source = ./storage/ssh-config;
     target = ".ssh/config";
   };
-  home.packages = with pkgs; [
-    sshfs # Utility to mount SSH connection as filesystem
-  ];
 
   services.syncthing = {
     enable = true;
@@ -53,41 +50,4 @@
     };
   };
 
-  systemd.user.automounts."home-${username}-home_server" = {
-    Unit = {
-      Description = "Automount ~/home_server";
-    };
-
-    Automount = {
-      Where = "/home/${username}/home_server";
-      TimeoutIdleSec = "5min";
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-  
-  systemd.user.mounts."home-${username}-home_server" = {
-    Unit = {
-      Description = "SSHFS (Home Server)";
-      Before = "remote-fs.target";
-    };
-
-    Mount = {
-      What = "${username}@nixos.lan:/mnt/storage";
-      Where = "/home/${username}/home_server";
-      Type = "fuse.sshfs";
-      Options = [ "_netdev" "rw" "nosuid" "default_permissions" "allow_other" "follow_symlinks" "idmap=user" "ServerAliveInterval=10" "ServerAliveCountMax=1" "ConnectTimeout=3" ];
-      ForceUnmount = true;
-      DirectoryMode = "0700";
-      TimeoutSec = 10;
-    };
-
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
 }
-
