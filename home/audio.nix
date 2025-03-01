aacc1a119fbb8cdb7a66283c2222aa78724afb75
Géza Búza <bghome@gmail.com>
2025-03-01 18:43:11 +0100

    Install Nicotine+

--- home/audio.nix
+++ home/audio.nix
@@ -4,6 +4,7 @@
     audacious # Winmap alike music player
     audacity # Audio editor
     gapless # Lightweight GTK4 music player (a.k.a. g4music)
+    nicotine-plus # A graphical client for Soulseek
     picard # Music file tagger
     streamrip # Music downloader
   ];
