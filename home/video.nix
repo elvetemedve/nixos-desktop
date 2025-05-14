f615fbf12f1fd2f7d8875ad7e7f5d58b34fe155d
Géza Búza <bghome@gmail.com>
2025-05-14 22:11:27 +0200

    Install MPV media player

--- home/video.nix
+++ home/video.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    mpv # General-purpose media player, fork of MPlayer and mplayer2
     webtorrent_desktop # Play videos by streaming from torrent files
   ];
 }
