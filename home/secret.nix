38813a28915d2637b44b701e9ee1cc3390621d01
Géza Búza <bghome@gmail.com>
2025-08-15 22:24:15 +0200

    Adjust configuration of KeePassXC
    Start KeePassXC automatically after login.
    Install Magic Wormhole CLI app.

--- home/secret.nix
+++ home/secret.nix
@@ -1,7 +1,28 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    keepassxc # KeePassXC offline password manager
+    magic-wormhole # Securely transfer data between computers
   ];
+
+  # KeePassXC offline password manager
+  programs.keepassxc = {
+    autostart = true;
+    enable = true;
+    settings = {
+      Browser.Enabled = true;
+      Browser.UpdateBinaryPath = false;
+
+      GUI = {
+        AdvancedSettings = true;
+        ApplicationTheme = "dark";
+        CompactMode = true;
+        HidePasswords = true;
+      };
+
+      SSHAgent.Enabled = true;
+    };
+  };
+
+  xdg.autostart.enable = true; # Whether to enable creation of XDG autostart entries.
 }
 
