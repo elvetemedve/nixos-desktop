6e4b065f6b6839ac4a97e9e0158c9aaedb93912e
Géza Búza <bghome@gmail.com>
2025-11-16 12:43:10 +0100

    Setup Looking Glass host dependencies for VM

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -3,6 +3,8 @@
 { config, pkgs, username, ... }:
 
 {
+  boot.extraModulePackages = [ config.boot.kernelPackages.kvmfr ]; # Install the kernel module for Looking Glass communication
+
   boot.kernelPackages = pkgs.linuxPackages_latest; # Use the latest stable kernel version
 
   boot.kernelParams = [ 
@@ -10,6 +12,7 @@
     "iommu=pt"
     "amdgpu.runpm=0" # Workaround for a power management bug with GMKtec AD-GP1 AMD Radeon 7600M XT
     "vfio-pci.ids=1002:7480,1002:ab30"
+    "kvmfr.static_size_mb=256" # Load the kernel modul for the host of Looking Glass
   ];
 
   boot.initrd.kernelModules = [
@@ -18,12 +21,15 @@
     "vfio_iommu_type1"
 
     "i915"
+
+    "kvmfr"
   ];
 
   # Workaround for power management issues when vfio-pci driver is used
   # Optional: ATTR{power/control}="on"
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x1002", ATTR{device}=="0x7480", ATTR{d3cold_allowed}="0"
+    SUBSYSTEM=="kvmfr", OWNER="qemu-libvirtd", GROUP="kvm", MODE="0660"
   '';
 
   # Enable libvirt and KVM virtualization
@@ -41,6 +47,7 @@
 
   # Install virt-manager and related tools
   environment.systemPackages = with pkgs; [
+    looking-glass-client
     virt-manager
     virt-viewer
     virtio-win  # VirtIO drivers
