8c5496c4134159e6d89b52a2160069e9daa9c7c3
Géza Búza <bghome@gmail.com>
2026-03-28 17:28:30 +0100

    Install Veracrypt

--- modules/security.nix
+++ modules/security.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}:
 
 {
+  environment.systemPackages = with pkgs; [
+    veracrypt # Free Open-Source filesystem on-the-fly encryption
+  ];
   services.udev.packages = [ pkgs.nitrokey-udev-rules ]; # Add Udev rules to recognize connected NitroKey devices
 }
