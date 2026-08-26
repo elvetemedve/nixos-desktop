e59a4ba8493e8e8ddca909f9762e84e2df8d25ee
Géza Búza <bghome@gmail.com>
2026-08-26 15:51:19 +0200

    Fix resume from suspend

--- hosts/ThinkPadP16s/default.nix
+++ hosts/ThinkPadP16s/default.nix
@@ -12,6 +12,10 @@
 
   networking.hostName = "ThinkPadP16s"; # Define your hostname.
 
+  # nouveau fails to init the eGPU's GSP firmware and can wedge suspend/resume
+  # (s2idle) if the eGPU is unplugged while the laptop is asleep.
+  boot.blacklistedKernelModules = [ "nouveau" ];
+
   services.udev.extraRules = ''
     # AlphaTheta / Pioneer DJ controllers: let the seated user open the HID node
     SUBSYSTEM=="hidraw", ATTRS{idVendor}=="2b73", GROUP="users", MODE="0660", TAG+="uaccess"
