aa87f81d4d877e6549bd3931a6038930fd61aae4
Géza Búza <bghome@gmail.com>
2026-09-03 00:47:11 +0200

    LibreOffice upstream has changed the versioning to `libreoffice-stable`

--- home/document.nix
+++ home/document.nix
@@ -3,6 +3,6 @@
   home.packages = with pkgs; [
     # LibreOffice free and private office suite.
     # Includes Writer (word processing), Calc (spreadsheets), Impress (presentations), Draw (vector graphics and flowcharts), Base (databases), and Math (formula editing).
-    libreoffice-fresh 
+    libreoffice-stable 
   ];
 }
